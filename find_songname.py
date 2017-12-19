6e9fd93e8d0de493c5dc747de6812cff94a28c34
tianwei1992 <879983690@qq.com>
2017-12-19 16:02:45 +0800

    find_songname.py基础代码写完了，也测试通过，只是TK界面太丑

--- find_songname.py
+++ find_songname.py
@@ -1,41 +1,68 @@
 from tkinter import *
+import tkinter.messagebox as messagebox
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
+class ChooseApp(Tk):
+	def __init__(self,song_lst):
+		super().__init__()
+		self.sonng_lst=song_lst
+		self.seq=IntVar()
+		self.title('input_sequence')
+		self.resizable(1, 0)  # 不懂
+		self.setup_UI(song_lst)
+	def setup_UI(self,song_lst):
+		#self.geometry('300x500')
+		# 在root内创建第1行……
+		row1 = Frame(self)
+		row1.pack(fill="x",side=LEFT)
+		Label(row1, text='找到相关歌曲'+str(len(song_lst))+'首，请选择：', width=28).pack(side=LEFT)
+		#第2行
+		row2 = Frame(self)
+		row2.pack(fill="x")
+		self.r = StringVar()
+		#self.r保存选择结果
+		for i,song in enumerate(song_lst):
+			print(i,song)
+			#value是当前选项的特征值
+			self.radio = Radiobutton(self, variable=self.r, value=i, text='{0}：{1} by {2}({3})'.format(i,song[1],song[2],song[3]),command=self.sel,activebackground='Lavender',activeforeground='grey',bg='ghostwhite',fg='Orchid')
+			#正常情况下：字体颜色fg，背景色bg
+			#选中：字体activeforeground，背景activebackground
+			self.radio.pack()
 
-"""
-还是用正则，这种方式拿不到songid
-print('key_infos========',type(key_infos.text))#str
-print('key_infos========', key_infos.text.split("\n"))
+		#第三行
+		row3 = Frame(self)
+		row3.pack(fill="x")
+		Button(row1, text="取消", command=self.cancel).pack(side=RIGHT)
+		Button(row1, text="提交", command=self.ok).pack(side=RIGHT)
+		#第4行，
+		self.label = Label(self)
+		self.label.pack()
+	def sel(self):
+		selection = "你选择了第{0}首歌".format(str(self.r.get()))
+		self.label.config(text=selection)
 
-info_lst=key_infos.text.split("\n")
+	def cancel(self):
+		self.destroy()
+	def ok(self):
+		print(self.r.get())
+		self.destroy()
+		return
 
-song_lst=[]
-for i in range(len(info_lst)):
-	if i%4==0:
-		song_dic={}
-		song_dic['name']=info_lst[i]
-	elif i % 4 == 1:
-		song_dic['singer']=info_lst[i]
-	elif i % 4 == 2:
-		song_dic['album']=info_lst[i]
-	else:
-		song_dic['lenth']=info_lst[i]
-		song_lst.append(song_dic)
-print(song_lst)
-"""
-
-
-#要把这个list用tk给用户看，用户选择其中的某一个
 def users_choice(song_lst):
-	return 1
+	print("请选择:")
+	root=ChooseApp(song_lst)
+	root.mainloop()
+	return int(root.r.get())
 
 #return item
 #先假设用户选择了第一个
 
 def find_songname(songname):
 	browser = webdriver.Chrome()
+	#最小化窗口，因为用户不关心过程
+	browser.minimize_window()
 	url = 'http://music.163.com/#/search/m/'
 	browser.get(url)
 	#先定位到g-iframe这个frame
@@ -60,15 +87,60 @@ def find_songname(songname):
 	for song in song_lst:
 		print(song)
 	#呈现给用户，让用户选择
+	print('调用tk展示song_lst,让用户选择')
 	song_seq=users_choice(song_lst)
+	print('在tk中，用户选择了：',song_seq)
 	song_id = song_lst[song_seq][0]
 	return song_id
 
 
+class InputApp(Tk):
+	def __init__(self):
+		super().__init__()
+		self.song_name = StringVar()
+		# 2指定root属性
+		self.title('input_songname')
+		self.resizable(1, 0)#不懂
+		self.geometry('180x60')
+		self.setup_UI()
+	def setup_UI(self):
+	# 3在root内创建第1行……
+		row1 = Frame(self)
+		row1.pack(fill="x")
+		Label(row1, text='请输入歌名：', width=12).pack(side=LEFT)
+		Entry(row1, textvariable=self.song_name, width=20).pack(side=LEFT)
+	#第2行
+		row2 = Frame(self)
+		row2.pack(fill="x")
+		#这个顺序保证"确定"在左
+		Button(row2, text="取消", command=self.cancel).pack(side=RIGHT)
+		btn2=Button(row2, text="确定", command=self.ok).pack(side=RIGHT)
+	def cancel(self):
+		self.destroy()
+	def ok(self):
+		# 返回输入
+		assert self.song_name,"输入为空"
+		song_name=self.song_name.get()
+		self.destroy()
+
+
+def input_songname():
+	#1创建root主窗口
+	root = InputApp()
+	root.mainloop()#为什么开两个
+	song_name=root.song_name.get()
+	return song_name
+
+
+
 if __name__=='__main__':
-	songname='路口'
-	song_id=find_songname(songname)
-	print("with songname='路口',song_id=",song_id)
-	songname='鼓楼'
-	song_id=find_songname(songname)
-	print("with songname='鼓楼',song_id=",song_id)
+	print('请输入歌名')
+	song_name=input_songname()
+	#print('用户已输入：',song_name)
+	print("下面开始查找相关歌曲")
+	song_id=find_songname(song_name)
+	print("已经拿到songid,song_id=",song_id)
+	#songname='鼓楼'
+	#song_id=find_songname(songname)
+	#print("with songname='鼓楼',song_id=",song_id)
+	#users_choice([[1,2,3,4],[5,6,7,8]])
